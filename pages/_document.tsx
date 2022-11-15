81640ed5143141ca488d27f9a5eb7aa87077ee55
Pablo Weisbek <pablo.weisbek@gmail.com>
2022-11-14 21:58:25 -0300

    fixes fonts import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={''} />
-        <link href="https://fonts.googleapis.com/css2?family=Alkalami&display=swap" rel="stylesheet" /> */}
+        <link href="https://fonts.googleapis.com/css2?family=Alkalami&display=swap" rel="stylesheet" /> 
+        <link href="https://fonts.googleapis.com/css2?family=Six+Caps&display=swap" rel="stylesheet" /> 
       </Head>
       <body>
         <Main />
