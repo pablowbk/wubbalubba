c5d131785021c263278ef9205c0d440768597992
Pablo Weisbek <pablo.weisbek@gmail.com>
2022-11-18 07:26:55 -0300

    adds 404

---
+++ pages/404.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next"
+
+const styles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  rowGap: '2rem',
+  minHeight: '100vh',
+  padding: 'auto 10vmin',
+  flexDirection: 'column' as 'column',
+}
+
+const NotFound: NextPage = () => {
+  return (
+    <div style={styles}>
+      <p>Something went wrong while fetching your data. This things happen, y'know?</p>
+      <p>Verify that you're trying to access the right page.</p>
+    </div>
+  )
+}
+
+export default NotFound
